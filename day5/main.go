8fff098beb39c159109956e65cad0fae0c28fc66
Thanoj Singh <thanoj.singh@gmail.com>
2023-12-15 01:21:30 -0500

    day 5, slow

--- day5/main.go
+++ day5/main.go
@@ -27,7 +27,7 @@ func main() {
 	lines := strings.Split(content, "\r\n")
 	first := capture(`seeds: ([\d\s]*)$`, lines[0])
 
-	seeds := digitize(splitTrim(first, " "))
+	seedPairs := digitize(splitTrim(first, " "))
 
 	amaps := []adventMap{}
 
@@ -57,30 +57,45 @@ func main() {
 		}
 	}
 
-	endings := []int{}
-	for _, seed := range seeds {
-		current := seed
-		for _, amap := range amaps {
-			for _, arange := range amap.ranges {
-				sourceMax := arange.source + arange.run - 1
-				if current >= arange.source && current <= sourceMax {
-					difference := current - arange.source
-					current = arange.destination + difference
-					break
+	smallest := 0
+
+	for i := 0; i < len(seedPairs); i += 2 {
+		fmt.Println("starting a seed pair")
+		start := seedPairs[i]
+		run := seedPairs[i+1]
+		end := start + run - 1
+
+		for seed := start; seed <= end; seed++ {
+			current := seed
+			for _, amap := range amaps {
+				for _, arange := range amap.ranges {
+					sourceMax := arange.source + arange.run - 1
+					if current >= arange.source && current <= sourceMax {
+						difference := current - arange.source
+						current = arange.destination + difference
+						break
+					}
 				}
 			}
+
+			if smallest == 0 {
+				smallest = current
+				continue
+			}
+
+			if current < smallest {
+				smallest = current
+			}
 		}
-		fmt.Println("started at", seed, "ended at", current)
-		endings = append(endings, current)
 	}
 
-	smallest := endings[0]
-	for _, ending := range endings {
-		if ending < smallest {
-			smallest = ending
-		}
-	}
-	fmt.Println(seeds)
+	//smallest := endings[0]
+	//for _, ending := range endings {
+	//	if ending < smallest {
+	//		smallest = ending
+	//	}
+	//}
+	fmt.Println(seedPairs)
 	fmt.Println(amaps)
 	fmt.Println("smallest ending", smallest)
 }
