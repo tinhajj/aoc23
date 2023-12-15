0e6bd45b214ec22bea868817c7e12c18c51f9678
Thanoj Singh <thanoj.singh@gmail.com>
2023-12-15 01:47:37 -0500

    day 6

--- day6/main.go
+++ day6/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+type race struct {
+	time   int
+	record int
+}
+
+var races = []race{
+	{59796575, 597123410321328},
+}
+
 func main() {
-	fmt.Println("start")
+	raceWays := []int{}
+	for _, race := range races {
+		won := 0
+		for charge := 0; charge < race.time; charge++ {
+			distance := (race.time - charge) * charge
+			if distance > race.record {
+				won++
+			}
+		}
+		raceWays = append(raceWays, won)
+	}
+
+	product := 1
+	for _, raceWays := range raceWays {
+		product = product * raceWays
+	}
+	fmt.Println(product)
 }
